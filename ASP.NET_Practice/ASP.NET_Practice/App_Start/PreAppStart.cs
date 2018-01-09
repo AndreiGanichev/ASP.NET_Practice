35afbee37e6903d4dd59f37a9de38dab8626dbc6
AndreiGanichev <aganichev10@gmail.com>
2018-01-09 21:49:24 +0300

    1. Add WebActivator and  pre App_Start method 2. Write logs in Global.asax

---
+++ ASP.NET_Practice/ASP.NET_Practice/App_Start/PreAppStart.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ASP.NET_Practice.App_Start.PreAppStart), "PreStart")]
+namespace ASP.NET_Practice.App_Start
+{
+    public static class PreAppStart
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public static void PreStart()
+        {
+            _logger.Info("Application PreStart");
+        }
+    }
+}
--- ASP.NET_Practice/ASP.NET_Practice/Global.asax.cs
+++ ASP.NET_Practice/ASP.NET_Practice/Global.asax.cs
@@ -4,15 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using NLog;
 
 namespace ASP.NET_Practice
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Вызывается, когда первый экземпляр HttpApplication создается. Это позволяет создавать объекты доступные для всех экземпляров класса HttpApplication.
+        /// </summary>
         protected void Application_Start()
         {
+            _logger.Info("Application starting");
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        /// <summary>
+        /// Приложение инициализируется или при первом вызове. Вызывается для всех экземпляров объекта HttpApplication.
+        /// </summary>
+        public override void Init()
+        {
+            base.Init();
+            _logger.Info("Application Init");
+        }
+
+        /// <summary>
+        /// Вызывается непосредственно перед уничтожением объекта HttpApplication
+        /// </summary>
+        public override void Dispose()
+        {
+            base.Dispose();
+            _logger.Info("Application Dispose");
+        }
+
+        /// <summary>
+        ///  Вызывается, когда необработанное исключение случается в приложении
+        /// </summary>
+        protected void Application_Error()
+        {
+            _logger.Info("Application Error");
+        }
+
+        /// <summary>
+        /// Вызывается, когда все созданные ранее экземпляры класса HttpApplication уничтожены (только однажды в течение всего времени жизни приложения).
+        /// </summary>
+        protected void Application_End()
+        {
+            _logger.Info("Application End");
         }
     }
 }
