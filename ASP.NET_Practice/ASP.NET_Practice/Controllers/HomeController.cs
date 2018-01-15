e665118446fdf3fa158a2361a07d534015ea1e3d
AndreiGanichev <aganichev10@gmail.com>
2018-01-15 08:23:29 +0300

    Add common controller and view and move there PracticeRepository usage

---
+++ ASP.NET_Practice/ASP.NET_Practice/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ASP.NET_Practice.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ASP.NET_Practice.Controllers
+{
+    public class HomeController : RepositoryBaseController
+    {
+        //при создании экземпляра контроллера исходя из настроек контейнера
+        //будут проинициализирован аргумент конструктора weapon
+        public HomeController(IWeapon weapon)
+        {
+            Weapon = weapon;
+        }
+
+        public IWeapon Weapon { get; set; }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            foreach (var role in Repository.Roles)
+            {
+                var name = role.Name;
+            }
+            return View(Repository.Roles.ToList());
+        }
+    }
+}
---
+++ ASP.NET_Practice/ASP.NET_Practice/Controllers/RepositoryBaseController.cs
@@ -0,0 +1,13 @@
+﻿using ASP.NET_Practice.DataAccess.Interfaces;
+using Ninject;
+using System.Web.Mvc;
+
+namespace ASP.NET_Practice.Controllers
+{
+    public class RepositoryBaseController : Controller
+    {
+        //будет проинициализировано при создании экземпляра контроллера исходя из настроек контейнера
+        [Inject]
+        public IPracticeRepository Repository { get; set; }
+    }
+}
